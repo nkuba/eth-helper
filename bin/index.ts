877e00f4ab9d3262f388d9899b03b4e0337892c8
Jakub Nowakowski <jakub.nowakowski@akena.co>
2022-08-24 09:14:19 +0200

    Replace yargs with commander

--- bin/index.ts
+++ bin/index.ts
@@ -1,54 +1,51 @@
-#!/usr/bin/env ts-node
+#!/usr/bin/env node
 
-import yargs from "yargs"
-import { hideBin } from "yargs/helpers"
-import { decrypt } from "../lib/decrypt"
+import { Command } from "commander"
+const program = new Command()
+
+import { decryptKeyFile } from "../lib"
 
 import fs from "fs"
 
 import type { EncryptedKeystoreV3Json } from "web3-core/types"
 
-yargs(hideBin(process.argv))
-  .scriptName("eth-helper")
-  .usage("$0 <cmd> [args]")
-  .command(
-    "extract-private-key",
-    "extracts private key from ethereum key file",
-    (yargs) => {
-      yargs
-        .option("key-file-path", {
-          alias: "k",
-          demandOption: true,
-          type: "string",
-        })
-        .option("password-file-path", {
-          alias: "p",
-          demandOption: true,
-          type: "string",
-        })
-        .option("output", {
-          alias: "o",
-          demandOption: true,
-          type: "string",
-        })
-    },
-    (argv) => {
-      const keyFilePath: string = argv.keyFilePath as string
-      const passwordFilePath: string = argv.passwordFilePath as string
-      const outputFilePath: string = argv.output as string
+program
+  .name("eth-helper")
+  .description("CLI for Ethereum Helpers.")
+  .version("0.0.1")
 
-      console.log(`extracting private key from key file: ${keyFilePath}`)
-
-      const keyFile: EncryptedKeystoreV3Json = JSON.parse(
-        fs.readFileSync(keyFilePath, "utf8")
-      )
-      const password = fs.readFileSync(passwordFilePath, "utf8")
-
-      const { privateKey } = decrypt(keyFile, password)
-
-      fs.writeFileSync(outputFilePath, privateKey)
-
-      console.log(`private key stored in file: ${outputFilePath}`)
-    }
+program
+  .command("extract-private-key")
+  .description("Extracts private key from ethereum key file.")
+  .requiredOption(
+    "-k --key-file-path <path>",
+    "path to the encrypted ethereum key file"
   )
-  .help().argv
+  .requiredOption(
+    "-p --password-file-path <path>",
+    "path to the file containing password to the ethereum key file"
+  )
+  .requiredOption(
+    "-o --output <path>",
+    "path to a file where an extracted private key will be stored"
+  )
+  .action((name, options, command) => {
+    const keyFilePath: string = options.keyFilePath as string
+    const passwordFilePath: string = options.passwordFilePath as string
+    const outputFilePath: string = options.output as string
+
+    console.log(`extracting private key from key file: ${keyFilePath}`)
+
+    const keyFile: EncryptedKeystoreV3Json = JSON.parse(
+      fs.readFileSync(keyFilePath, "utf8")
+    )
+    const password = fs.readFileSync(passwordFilePath, "utf8")
+
+    const { privateKey } = decryptKeyFile(keyFile, password)
+
+    fs.writeFileSync(outputFilePath, privateKey)
+
+    console.log(`private key stored in file: ${outputFilePath}`)
+  })
+
+program.parse(process.argv)
